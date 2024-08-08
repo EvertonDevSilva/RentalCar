d0e68f529b8efd383a1d16d09831c2fc967bf266
EvertonDevSilva <dev.everton@gmail.com>
2024-08-08 20:50:41 -0300

    Car Service created

---
+++ RentalCar/Services/CarService.cs
@@ -0,0 +1,34 @@
+﻿using RentalCar.Interfaces;
+using RentalCar.Models;
+
+namespace RentalCar.Services
+{
+    internal class CarService(ICarRepository carRepository) : ICarService
+    {
+        private readonly ICarRepository _carRepository = carRepository;
+        public Task Create(Car car)
+        {
+            return _carRepository.Create(car);
+        }
+
+        public Task<Car> GetById(int id)
+        {
+            return _carRepository.GetById(id);
+        }
+
+        public Task<List<Car>> GetAll()
+        {
+            return _carRepository.GetAll();
+        }
+
+        public Task Update(Car car)
+        {
+            return _carRepository.Update(car);
+        }
+
+        public Task Delete(int id)
+        {
+            return _carRepository.Delete(id);
+        }
+    }
+}
